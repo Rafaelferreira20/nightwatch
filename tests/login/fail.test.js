640f8f9002affb5d2887fe1636d4235db45e6c52
rafaelferreira2 <sf.rafael20@gmail.com>
2020-03-23 21:14:02 -0400

    adicionando hooks global

--- tests/login/fail.test.js
+++ tests/login/fail.test.js
@@ -1,15 +1,5 @@
 
 module.exports = {
-
-    before: (browser) => {
-        browser.resizeWindow(1920, 1080)
-
-    },
-    after: (browser) => {
-        browser.end();
-    },
-
-
     'senha incorreta': (browser) => {
         let login = browser.page.login()
         login
--- tests/login/sucesso.test.js
+++ tests/login/sucesso.test.js
@@ -5,13 +5,10 @@ module.exports = {
         let login = browser.page.login()
         let sidebar = browser.page.sidebar()
 
-        browser.resizeWindow(1920, 1080)
-
         login.with('sf.rafael20@gmail.com', 'teste652')
 
         sidebar
             .waitForElementVisible('@userInfo', 3000)
             .assert.containsText('@userInfo', 'Rafael Ferreira')
-            .end();
     }
 }
